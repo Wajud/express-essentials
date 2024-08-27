b904ee683377878328909cba05dd4eb361c79f2b
Wajud <wajudkareem757@gmail.com>
2024-08-27 19:39:26 +0100

    end of chapter 3: Routing

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-import express from "express";
+import express, { response } from "express";
 import data from "./data/mock.json" assert { type: "json" };
 
 const app = express();
@@ -10,9 +10,45 @@ app.use(express.static("public"));
 //Using the images folder at the route /images
 app.use("/images", express.static("images"));
 
+// GET - redirect method
+app.get("/redirect", (request, response) => {
+  response.redirect("https://www.linkedin.com/in/wajud-kareem-92537115a/");
+});
+
+app
+  .route("/class")
+  .get((request, response) => {
+    response.send("Retrieve class info");
+  })
+  .post((request, response) => {
+    response.send("Create class info");
+  })
+  .put((request, response) => {
+    response.send("Edited");
+  });
+
 //GET
 app.get("/", (request, response) => {
   response.json(data);
+});
+
+//GET with next()
+app.get(
+  "/tonext",
+  (request, response, next) => {
+    console.log("The response will be sent by the next function");
+    next();
+  },
+  (request, response) => {
+    response.send("I just set up a route with a second callback");
+  }
+);
+
+//GET with routing parameters
+app.get("/class/:studentId", (request, response) => {
+  const studentId = Number(request.params.studentId);
+  const student = data.filter((student) => student.id === studentId);
+  response.send(student);
 });
 
 //POST
@@ -31,6 +67,6 @@ app.delete("/delete", (request, response) => {
 });
 
 app.listen(PORT, () => {
-  console.log(data);
+  // console.log(data);
   console.log(`The server is running on PORT: ${PORT}`);
 });
